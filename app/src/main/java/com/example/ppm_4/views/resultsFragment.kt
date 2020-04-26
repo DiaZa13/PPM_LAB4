d96b617ca810b070c33d1e2905065600eb619caf
Diana Corado <cor191025@uvg.edu.gt>
2020-04-26 14:56:14 -0600

    Almost done

--- app/src/main/java/com/example/ppm_4/views/resultsFragment.kt
+++ app/src/main/java/com/example/ppm_4/views/resultsFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 
 import com.example.ppm_4.R
+import com.example.ppm_4.databinding.FragmentResultsBinding
+import com.example.ppm_4.databinding.FragmentStartBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +22,12 @@ class resultsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_results, container, false)
+        val binding: FragmentResultsBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_start, container, false)
+        binding.btnReload.setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_resultsFragment_to_registerFragment)
+        }
+        return binding.root
     }
 
 }
